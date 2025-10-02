77d5b9d74e469936665283bb2b5c5f1182902d05
Ameer Hamza <hamzanaseer496@gmail.com>
2025-10-02 13:29:15 +0500

    update

--- server/Utils/sendEmail.js
+++ server/Utils/sendEmail.js
@@ -6,7 +6,7 @@ const sendEmail = async (to, subject, htmlContent) => {
     const transporter = nodemailer.createTransport({
       host: process.env.EMAIL_HOST || "smtp.gmail.com",
       port: process.env.EMAIL_PORT || 587,
-      secure: true, // 465 => true, 587 => false
+      secure: false, // 465 => true, 587 => false
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
